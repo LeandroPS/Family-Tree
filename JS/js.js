71a46d38d5e05a6f6f1ca4f1ccbc4ff7eeec58d3
Leandro Pires de Souza <leandro.pires.souza@outlook.com>
2015-06-02 14:23:23 -0400

    Adding Zoom and Drag behavior
    This will make it easier to visualize and navigate through the branches
    when the tree gets more "heavy"

--- JS/js.js
+++ JS/js.js
@@ -5,6 +5,7 @@ d3.json("JS/Tree.json", function(json) {
     var margin = {top: 0, right: 320, bottom: 0, left: 0},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
+    
 
     var tree = d3.layout.tree()
         .separation(function(a, b) { return a.parent === b.parent ? 1 : .5; })
@@ -12,24 +13,36 @@ d3.json("JS/Tree.json", function(json) {
             return d.parents; 
         })
         .size([height, width]);
+    
+    var zoom = d3.behavior.zoom()
+        .scaleExtent([1, 10])
+        .on("zoom", zoomed);
+    
+    var drag = d3.behavior.drag()
+        .origin(function(d) { return d; })
+        .on("dragstart", dragstarted)
+        .on("drag", dragged)
+        .on("dragend", dragended);
 
     var svg = d3.select("body").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
-        .append("g")
+        .call(zoom);
+        
+    var container = svg.append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     
     var nodes = tree.nodes(json);
     console.log(json);
 
-    var link = svg.selectAll(".link")
+    var link = container.selectAll(".link")
         .data(tree.links(nodes))
         .enter().append("path")
         .attr("class", "link")
         .attr("d", elbow);
 
-    var node = svg.selectAll(".node")
+    var node = container.selectAll(".node")
         .data(nodes)
         .enter().append("g")
         .attr("class", "node")
@@ -67,5 +80,22 @@ d3.json("JS/Tree.json", function(json) {
         return "M" + d.source.y + "," + d.source.x
             + "H" + d.target.y + "V" + d.target.x
             + (d.target.children ? "" : "h" + margin.right);
+    }
+    
+    function zoomed() {
+      container.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+    }
+    
+    function dragstarted(d) {
+      d3.event.sourceEvent.stopPropagation();
+      d3.select(this).classed("dragging", true);
+    }
+
+    function dragged(d) {
+      d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+    }
+
+    function dragended(d) {
+      d3.select(this).classed("dragging", false);
     }
 });
