e50115191715a66c3311a95bf384646027bec347
Leandro Pires de Souza <leandro.pires.souza@outlook.com>
2015-06-03 14:51:26 -0400

    8K

--- JS/js.js
+++ JS/js.js
@@ -1,10 +1,21 @@
-
+var max = 0;
+var maxdepth = 0;
 
 d3.json("JS/Tree.json", function(json) {
     
     var margin = {top: 0, right: 0, bottom: 0, left: 0},
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+        /*width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;*/
+        width = window.innerWidth,
+        height = window.innerHeight;
+    
+    
+    function counter(d){
+        max = max+1;
+        if(d.depth>maxdepth){
+            maxdepth = d.depth;
+        }
+    }
     
 
     var tree = d3.layout.tree()
@@ -14,7 +25,7 @@ d3.json("JS/Tree.json", function(json) {
         .children(function(d) { 
             return d.parents; 
         })
-        .size([height+1500, width]);
+        .size([height*128, width*2]);
     
     var zoom = d3.behavior.zoom()
         .scaleExtent([-2, 10])
@@ -36,13 +47,17 @@ d3.json("JS/Tree.json", function(json) {
 
     
     var nodes = tree.nodes(json);
-    console.log(json);
+    //console.log(json);
     
     //
     
     var fontScale = d3.scale.linear()
-        .domain([0, 7])
-        .range([15,5]);
+        .domain([0, 12])
+        .range([12,4]);
+    
+    var spaceScale = d3.scale.linear()
+        .domain([0, 12])
+        .range([-6, -1]);
 
     var link = container.selectAll(".link")
         .data(tree.links(nodes))
@@ -55,14 +70,15 @@ d3.json("JS/Tree.json", function(json) {
         .enter().append("g")
         .attr("class", "node")
         .attr("transform", function(d) {
+            counter(d);
             console.log("x: "+d.x+" y: "+d.y+" depth: "+d.depth);
             return "translate(" + d.y + "," + d.x + ")"; 
         })
 
     node.append("text")
         .attr("class", "name")
-        .attr("x", 8)
-        .attr("y", -6)
+        .attr("x", 4)
+        .attr("y", function(d){return spaceScale(d.depth)})
         .attr("font-size", function(d){return fontScale(d.depth)+"px"})
         .text(function(d) { 
             return d.name; 
@@ -89,6 +105,13 @@ d3.json("JS/Tree.json", function(json) {
     //
     
     function elbow(d, i) {
+        /*
+        var diagonal = d3.svg.diagonal()
+            .projection(function(q) { return [q.y, q.x]; });
+        
+        var o = {x: d.source.x0, y: d.source.y0};
+        return diagonal({source: o, target: o});
+        */
         return "M" + d.source.y + "," + d.source.x
             + "H" + d.target.y + "V" + d.target.x
             + (d.target.children ? "" : "h" + margin.right);
